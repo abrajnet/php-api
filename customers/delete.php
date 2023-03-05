cff183e0dc758b788ae1b2f7a240b0b45aad0bc3
abrajnet <120955296+abrajnet@users.noreply.github.com>
2023-03-05 21:13:51 +0100

    Add files via upload

---
+++ customers/delete.php
@@ -0,0 +1,32 @@
+<?php
+
+header('Access-Control-Allow-Origin:chat.*');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: DELETE');
+header('Access-Control-Allow-Headers: Content - Type, Access -Control-Allow-Headers, Authorization, X-Request -With');
+
+include('function.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+
+if($requestMethod == "DELETE"){
+
+   
+        $deletecustomer = deleteCustomer($_GET);
+        echo $deletecustomer;
+  
+    
+}
+else{
+
+    $data = [
+         'status'=> 405,
+         'message' => $requestMethod. 'Method Not Allowed',
+    ];
+
+    header("HTTP/1.0 405 method not Allowed");
+    echo json_encode($data);
+}
+
+?>
